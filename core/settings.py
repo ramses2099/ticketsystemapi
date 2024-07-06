8eeb6185a600684d452c67960101acbaba687e11
ramses2099 <ramses2099@gmail.com>
2024-07-06 13:28:40 -0400

    changes

--- core/settings.py
+++ core/settings.py
@@ -62,9 +62,11 @@ CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200',
+    'http://127.0.0.1:4200',
 ] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 CORS_ALLOWED_ORIGIN_REGEXES = [
     'http://localhost:4200',
+    'http://127.0.0.1:4200',
 ]
 
 
