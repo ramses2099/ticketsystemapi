963d5bcffbc2699b6d5a5f0c923bd9d6b6c83a11
ramses2099 <ramses2099@gmail.com>
2024-07-06 12:50:05 -0400

    last changes

--- core/settings.py
+++ core/settings.py
@@ -60,6 +60,14 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
+] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    'http://localhost:4200',
+]
+
+
 
 ROOT_URLCONF = 'core.urls'
 
